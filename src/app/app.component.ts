e9077adabc1e64ab1eee9251305f214036c21fa8
Krzysztof Glinka <krzysztof.m.glinka@gmail.com>
2017-06-29 23:55:22 +0200

    more errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,4 +22,12 @@ export class AppComponent {
     this.taskList = new Array<task>();
     this.taskList.push(new task("dupa",false))
   }
+
+  setDone(item: task, done: boolean){
+    item.done = true;
+  }
+
+  delTask(item: task, done: boolean){
+    this.taskList.splice(this.taskList.indexOf(item),1)
+  }
 }
